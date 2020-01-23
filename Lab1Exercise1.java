f6030acd4ec67bcae158783a83ef43c0caec52f9
Shane Byrne <Shane.Byrne4@students.ittralee.ie>
2020-01-23 09:43:52 +0000

    New Files

---
+++ Lab1Exercise1.java
@@ -0,0 +1,11 @@
+//Lab1Exerciseone.java
+/*This program illustrates my name and address*/
+
+public class Lab1Exercise1 {
+		
+		public static void main(String args[])
+		{
+		
+		System.out.println("My name is Shane Byrne and I am from Abbeydorney");  //message being displayed
+    	}
+}
